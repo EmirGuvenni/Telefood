58573ac2b614dae948c842e00c33b59bdbf5d88b
Emir Güvenni <contact@emirguvenni.com>
2021-09-01 14:27:52 +0300

    Added graceful stopping

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { Telegraf } from "telegraf";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Graceful stop
+process.once("SIGINT", () => {
+  bot.stop();
+});
+process.once("SIGTERM", () => {
+  bot.stop();
+});
