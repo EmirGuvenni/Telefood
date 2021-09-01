442cb13104817ea5332e41319d8dc1d8c7c397b5
Emir Güvenni <contact@emirguvenni.com>
2021-09-01 15:41:08 +0300

    Added the webhook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Telegraf } from "telegraf";
 
+// Start the bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Start a webhook to listen for a incoming menu
+bot.telegram.setWebhook(process.env.WEBHOOK_URL);
 
 // Graceful stop
 process.once("SIGINT", () => {
